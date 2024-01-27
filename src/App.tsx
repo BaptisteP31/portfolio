a25c9f23f33089d3dd1dd546a9bf516073a9c139
PAQUERIAUD Baptiste <paqueriaudbaba@gmail.com>
2024-01-27 21:12:17 +0100

    Adds strictmode

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { MainSection } from "./components/MainSection/MainSection.tsx";
 import { Navabar } from "./components/Navbar/Navbar.tsx";
 import { AboutMeSection } from "./components/AboutMeSection/AboutMeSection.tsx";
 import { MyProjectSection } from "./components/MyProjectsSection/MyProjectsSection.tsx";
+import { StrictMode } from "react";
 
 const App = () => {
     return (
         <div>
-            <Navabar/>
-            <MainSection/>
-            <AboutMeSection/>
-            <MyProjectSection/>
-            <div style={{height: "2000px"}}></div>
+            <StrictMode>
+                <Navabar />
+                <MainSection />
+                <AboutMeSection />
+                <MyProjectSection />
+            </StrictMode>
         </div>
     );
 }
