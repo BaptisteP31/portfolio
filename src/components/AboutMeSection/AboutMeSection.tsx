774af27c98a737690954f3aead9d8773326121b7
PAQUERIAUD Baptiste <paqueriaudbaba@gmail.com>
2024-01-27 21:21:35 +0100

    Changes text in about me section

--- src/components/AboutMeSection/AboutMeSection.tsx
+++ src/components/AboutMeSection/AboutMeSection.tsx
@@ -3,6 +3,8 @@ import { Frame } from '../Frame/Frame';
 import './AboutMeSection.css';
 import IMAGES from './assets/images/images.tsx';
 
+const aboutMeText = 'ça c\'est moi (enfin, je suis pas en noir et blanc non plus). Je vais vous éviter la lecture d\'une longue tirade shakespeareenne sur ma vie, mais si vous voulez en savoir plus sur moi, vous pouvez aller voir mon CV juste en dessous.';
+
 export function AboutMeSection() {
     return (
         <>
@@ -12,7 +14,9 @@ export function AboutMeSection() {
                     <Frame components={[<img src={IMAGES.photoDeMoi}/>]} className='about-me-section-left' frameType='right'/>,
 
                     <p style={{maxWidth: '50%', margin: 'auto', textAlign: 'justify'}}
-                    >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae justo sed urna scelerisque volutpat. Sed vel urna ut nisl placerat aliquam. Donec vitae lectus sit amet magna aliquet convallis. Sed vitae justo sed urna scelerisque volutpat. Sed vel urna ut nisl placerat aliquam. Donec vitae lectus sit amet magna aliquet convallis.</p>
+                    >
+                        {aboutMeText}
+                    </p>
                 ]}
             />
         </>
