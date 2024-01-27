92a3cbc4debdecd70a8f20b4c6740c48d616b868
PAQUERIAUD Baptiste <paqueriaudbaba@gmail.com>
2024-01-27 14:36:46 +0100

    Adds separator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { MainSection } from "./components/MainSection/MainSection.tsx";
 import { Navabar } from "./components/Navbar/Navbar.tsx";
+import { Separator } from "./components/Separator/Separator.tsx";
 
 const App = () => {
     return (
         <div>
             <Navabar/>
             <MainSection/>
+            <Separator/>
             <div style={{height: "2000px"}}></div>
         </div>
     );
