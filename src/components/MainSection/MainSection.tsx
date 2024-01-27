b1c1fdcc5f5747312ebbbfb336f21d4c4865d74d
PAQUERIAUD Baptiste <paqueriaudbaba@gmail.com>
2024-01-27 21:18:32 +0100

    Changes text for main section

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -2,26 +2,28 @@ import './MainSection.css';
 import IMAGES from './assets/images/images';
 import { BasicSection } from '../BasicSection/BasicSection';
 
+const mainText = 'Bienvenue sur mon portfolio, je suis un développeur web fullstack junior. Je suis passionné par le développement back-end et depuis peu par le développement front-end. Je suis actuellement en formation dans l\'IUT d\'Aix-Marseille en BUT Informatique.';
+
 export function MainSection() {
     return (
         <>
             <BasicSection className='main-section container'
 
-            components={[
-                <div className='main-section-left'>
-                    <h1>
-                        paqueriaud <br/>
-                        baptiste
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae justo sed urna scelerisque volutpat. Sed vel urna ut nisl placerat aliquam. Donec vitae lectus sit amet magna aliquet convallis. Sed vitae justo sed urna scelerisque volutpat. Sed vel urna ut nisl placerat aliquam. Donec vitae lectus sit amet magna aliquet convallis.
-                    </p>
-                </div>,
-                
-                <div className='main-section-right container'>
-                    <img src={IMAGES.rightDecoration} alt='right-decoration'/>
-                </div>
-            ]}
+                components={[
+                    <div className='main-section-left'>
+                        <h1>
+                            paqueriaud <br />
+                            baptiste
+                        </h1>
+                        <p>
+                            {mainText}
+                        </p>
+                    </div>,
+
+                    <div className='main-section-right container'>
+                        <img src={IMAGES.rightDecoration} alt='right-decoration' />
+                    </div>
+                ]}
             />
 
         </>
